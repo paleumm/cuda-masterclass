e3f3d6d9a32896439b393450891312ad1370d9d8
paleumm <zdotqdot@gmail.com>
2023-06-06 15:06:54 +0700

    Update organize threads

--- Section1/organize_threads/kernel.cu
+++ Section1/organize_threads/kernel.cu
@@ -3,9 +3,22 @@
 
 #include <stdio.h>
 
-__global__ void print_threadIds() {}
+__global__ void print_threadIds() {
+	printf("threadIdx.x : %d, threadIdx.y : %d, threadIdx.z : %d\n", 
+		threadIdx.x, threadIdx.y, threadIdx.z);
+}
 
 int main() {
+	int nx, ny;
+	nx = 16;
+	ny = 16;
 
+	dim3 block(8, 8);
+	dim3 grid(nx / block.x, ny / block.y);
+
+	print_threadIds << <grid, block >> > ();
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
 	return 0;
 }
