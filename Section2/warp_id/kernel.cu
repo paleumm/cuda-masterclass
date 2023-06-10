839e850b44ef12a4ecd624fe0b7a421e099b0589
paleumm <zdotqdot@gmail.com>
2023-06-10 12:25:36 +0700

    Update warp details

--- Section2/warp_id/kernel.cu
+++ Section2/warp_id/kernel.cu
@@ -5,8 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void print_warp_details() {}
+__global__ void print_warp_details() {
+	int gid = blockIdx.y * gridDim.x * blockIdx.x + blockDim.x * blockIdx.x + threadIdx.x;
+	
+	int warp_id = threadIdx.x / 32;
+
+	// block index
+	int gbid = blockIdx.y * gridDim.x + blockIdx.x;
+
+	printf("tid: %d, bid.x: %d, bid.y: %d, gid: %d, warp_id: %d, gbid: %d\n", threadIdx.x, blockIdx.x, blockIdx.y, gid, warp_id, gbid);
+}
 
 int main() {
 
+	dim3 block(42);
+	dim3 grid(2, 2);
+
+	print_warp_details << <grid, block >> > ();
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return EXIT_SUCCESS;
 }
